6733fd7829970c1121df3845ea0b2b096ce9aaad
Vineet Kumar <139971679+Vineetkrsg@users.noreply.github.com>
2026-05-09 13:54:34 +0530

    Refactor add TwoNumbers function and linked list structure

--- Arrays/add_two_num.cpp
+++ Arrays/add_two_num.cpp
@@ -1,4 +1,5 @@
 /*
+Question:
 You are given two non-empty linked lists representing two non-negative integers.
 The digits are stored in reverse order, and each of their nodes contains a single digit.
 Add the two numbers and return the sum as a linked list.
@@ -18,63 +19,80 @@ Output: [0]
 Example 3:
 Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 Output: [8,9,9,9,0,0,0,1]
-
-Constraints:
-- The number of nodes in each linked list is in the range [1, 100].
-- 0 <= Node.val <= 9
-- It is guaranteed that the list represents a number that does not have leading zeros.
 */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <iostream>
+using namespace std;
 
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+// Definition of linked list node
+struct ListNode {
+    int val;
+    ListNode* next;
 
-        // Dummy node to simplify result list creation
-        ListNode* dummy = new ListNode(0);
-        ListNode* temp = dummy;
-
-        int carry = 0;
-
-        // Traverse both linked lists until all digits and carry are processed
-        while (l1 != NULL || l2 != NULL || carry != 0) {
-
-            int sum = carry;
-
-            // Add value from first list if available
-            if (l1 != NULL) {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-
-            // Add value from second list if available
-            if (l2 != NULL) {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-
-            // Update carry
-            carry = sum / 10;
-
-            // Create new node with current digit
-            ListNode* newNode = new ListNode(sum % 10);
-
-            // Attach node to result list
-            temp->next = newNode;
-            temp = temp->next;
-        }
-
-        // Return the actual result list
-        return dummy->next;
+    ListNode(int x) {
+        val = x;
+        next = NULL;
     }
 };
+
+// Function to add two numbers
+ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+    ListNode* dummy = new ListNode(0);
+    ListNode* temp = dummy;
+
+    int carry = 0;
+
+    while (l1 != NULL || l2 != NULL || carry != 0) {
+
+        int sum = carry;
+
+        if (l1 != NULL) {
+            sum += l1->val;
+            l1 = l1->next;
+        }
+
+        if (l2 != NULL) {
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        carry = sum / 10;
+
+        ListNode* newNode = new ListNode(sum % 10);
+
+        temp->next = newNode;
+        temp = temp->next;
+    }
+
+    return dummy->next;
+}
+
+// Function to print linked list
+void printList(ListNode* head) {
+    while (head != NULL) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+}
+
+int main() {
+
+    // First linked list: 2 -> 4 -> 3
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    // Second linked list: 5 -> 6 -> 4
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    // Add numbers
+    ListNode* result = addTwoNumbers(l1, l2);
+
+    // Print result
+    printList(result);
+
+    return 0;
+}
